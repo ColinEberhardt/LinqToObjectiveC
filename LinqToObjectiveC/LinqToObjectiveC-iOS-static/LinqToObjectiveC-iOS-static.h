3d93fefe4d7c993e1dcce36fadfdbf260555c95e
Alexander Dodatko <dodikk88.reg@gmail.com>
2015-03-14 15:41:42 +0200

    [carthage] Added dynamic framework target

---
+++ LinqToObjectiveC/LinqToObjectiveC-iOS-static/LinqToObjectiveC-iOS-static.h
@@ -0,0 +1,19 @@
+//
+//  LinqToObjectiveC-iOS-static.h
+//  LinqToObjectiveC-iOS-static
+//
+//  Created by dodikk on 3/14/15.
+//  Copyright (c) 2015 scottlogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LinqToObjectiveC-iOS-static.
+FOUNDATION_EXPORT double LinqToObjectiveC_iOS_staticVersionNumber;
+
+//! Project version string for LinqToObjectiveC-iOS-static.
+FOUNDATION_EXPORT const unsigned char LinqToObjectiveC_iOS_staticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinqToObjectiveC_iOS_static/PublicHeader.h>
+
+
